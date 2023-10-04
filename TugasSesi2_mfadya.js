c41fd2fe09b5539f0154664d127145777969eff1
fadyafadilah <114629375+fadyafadilah@users.noreply.github.com>
2023-10-04 07:08:37 +0700

    Update TugasSesi2_mfadya.js

--- TugasSesi2_mfadya.js
+++ TugasSesi2_mfadya.js
@@ -3,4 +3,7 @@ var lastName = "Amar Fadilah"
 var age = 20;
 var isMarried = false;
 
-
+console.log("Nama Depan :", firstName);
+console.log("Nama Belakang :",lasttName);
+console.log("Umur :", age);
+console.log("Status Pernikahan :", isMarried);
